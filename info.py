58b631afb7680d3751ce2ae59c6b8de27a43541f
Carrick S <carrickstopford@gmail.com>
2025-09-23 21:45:30 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,70 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I'm Carrick Stopford. Since my initials are CS, you could say that I'm made for what I do!"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/carrickstopcs/"
+my_github_url = "https://github.com/CarrickStop"
+my_email_address = "carrickstopford@gmail.com"
+
+
+education_data ={
+    'Degree': 'BS in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2029',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1552", "LMC 3511"], 
+    "names":["Intro to CS", "Integral Calculus", "American Literature and Culture"], 
+    "semester_taken":["1st", "1st", "1st"],
+    "skills":["Python, Streamlit, Pandas", "Calculus, Strong Logical Math Skills", "Communication, Reading Comprehension"],
+    }
+experience_data = {
+    "Information Technology and Repair Technician for Lifeline Repairs in Peachtree City, GA": (["- Repaired over 110 computers, tablets, and phones",
+                                                                          "- Diagnosed Countless Malfunctioning Devices", "- Accumulated a concrete understanding of computer hardware and design"],"Images/lifeline.jpeg"),
+    "PC Technician for Computech Systems in Fayetteville, GA":(["- Built PCs from scratch",
+                                                           "- Upgraded customer PCs for optimized performance"],"Images/computech.png")
+}
+
+projects_data = {
+    "Painless Pokédex": "Developed a Pokémon database API via webscraping and the Requests library",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 40
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕"
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🦅",
+    "German":"🍻"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "German": "Conversationally Acceptable"
+}
+award_data = {
+    "Whitewater High School in Fayetteville, GA": (["- Valedictorian out of a 310 class size"],"Images/speech.jpeg"),
+
+}
+ec_data={
+    "Georgia Tech in Atlanta, GA": ["- Georgia Tech Police Department intern, Student Government committee member, classic literature enthusiast"]
+}
