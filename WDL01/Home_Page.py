31748d4b9bcbd35e17d8ed2f77cc393f7aa8cd3a
Carrick S <carrickstopford@gmail.com>
2026-07-20 16:44:09 -0400

    refactor (about_me): now in homepage

--- WDL01/Home_Page.py
+++ WDL01/Home_Page.py
@@ -1,21 +1,16 @@
 import streamlit as st
 
 # Title of App
-st.title("Web Development Lab01")
+st.title("CS' Personal Website")
 
-# Assignment Data 
-# TODO: Fill out your team number, section, and team members
+#about me
+def about_me_section():
+    st.header("About me")
+    st.image(info.profile_picture, width = 200)
+    st.write(info.about_me)
+    st.write("---")
 
-st.header("CS 1301")
-st.subheader("Carrick Stopford")
-
-
-# Introduction
-# TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Page Name**: Description
-#       2. **Page Name**: Description
-#       3. **Page Name**: Description
-#       4. **Page Name**: Description
+about_me_section()
 
 st.write("""
 Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
