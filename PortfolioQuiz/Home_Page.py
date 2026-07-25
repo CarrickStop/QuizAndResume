16882dbfe6ef26a7baa318f8c6bdf0b9d786cdfd
Carrick S <carrickstopford@gmail.com>
2026-07-25 14:58:20 -0400

    chore: formatting changes to text

--- PortfolioQuiz/Home_Page.py
+++ PortfolioQuiz/Home_Page.py
@@ -11,13 +11,13 @@ st.title("CS' Personal Website")
 #about me
 def about_me_section():
     st.image(info.profile_picture, width = 200)
-    st.write(info.about_me)
+    st.write(f"**{info.about_me}**")
     st.write("---")
 
 about_me_section()
 
 st.write("""
-It's a pleasure to introduce myself! I'm an aspiring AI researcher and current Georgia Tech CS student. Please navigate to the pages on the side if you'd like to learn more about me.
+**It's a pleasure to introduce myself! I'm an aspiring AI researcher and current Georgia Tech CS student. Please navigate to the pages on the side if you'd like to learn more about me.**
 
 1. Portfolio: This page is where I showcase a sort of extended resume about myself. It includes basic resume aspects like education, experience, projects, and skills.
 2. Quiz: Having nothing to do with CS, I really enjoy classical literature as a side hobby. I would like to think it gives me an edge in communicative and writing based soft skills in the office, but here is a fun mini-quiz I put together on a few of my favorite pieces of literature.
