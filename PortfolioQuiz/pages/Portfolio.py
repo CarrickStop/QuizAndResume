bbe1b166eb2b17e320c48526483077f5edc5f6f2
Carrick S <carrickstopford@gmail.com>
2026-07-27 22:04:01 -0400

    chore: link change for resume

--- PortfolioQuiz/pages/Portfolio.py
+++ PortfolioQuiz/pages/Portfolio.py
@@ -105,6 +105,6 @@ def exporting():
     st.header("Download My Resume")
     res_expander = st.expander("Stopford_Carrick_Resume")
     res_expander.write("Let me guess: you're so blown away by this website that you want to hire me immediately? Here's a PDF of my resume:")
-    res_expander.write("https://drive.google.com/file/d/1mVBQeFMqY5Qr3IyaITQ0E1iadEIAQWQw/view?usp=sharing")    
+    res_expander.write("https://drive.google.com/file/d/1H-JLYZnBMHWR-4e164Mno4ET92tEj6dZ/view?usp=sharing")    
 
 exporting()
